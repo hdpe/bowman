8a1a168f5f7621760d71385ebbe78f9415c3a7fe
Ryan Pickett <ryan@hdpe.me>
2018-01-21 15:06:14 +0000

    Document new public API

--- client/src/main/java/uk/co/blackpepper/bowman/annotation/ResourceTypeInfo.java
+++ client/src/main/java/uk/co/blackpepper/bowman/annotation/ResourceTypeInfo.java
@@ -7,6 +7,14 @@ import java.lang.annotation.Target;
 
 import uk.co.blackpepper.bowman.TypeResolver;
 
+/**
+ * Class-level annotation to define the narrowing polymorphic deserialization strategy for
+ * properties (or property collection items) of this type.
+ * <p>
+ * Only one of {@link #subtypes} or {@link #typeResolver} should be specified.
+ * 
+ * @author Ryan Pickett
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ResourceTypeInfo {
@@ -15,7 +23,20 @@ public @interface ResourceTypeInfo {
 		// no members
 	}
 	
+	/**
+	 * The subtypes to consider in polymorphic deserialization of properties involving this type. On
+	 * deserialization, the final type of the property (or property collection item) will be determined 
+	 * by the self link of the resource.
+	 * 
+	 * @return the subtypes to consider in deserialization
+	 */
 	Class<?>[] subtypes() default {};
 	
+	/**
+	 * A custom narrowing type resolution strategy to use in polymorphic deserialization of properties
+	 * (or property collection items) of this type. 
+	 * 
+	 * @return the type resolver to use to determine the final type
+	 */
 	Class<? extends TypeResolver> typeResolver() default NullTypeResolver.class;
 }
