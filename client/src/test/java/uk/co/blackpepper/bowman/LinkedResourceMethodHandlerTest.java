444f2c1089d9e99d3b4ce7d660d50292479ac64c
andye <andye@yambay.com>
2018-06-06 22:30:39 +0800

    Converted to 4 space tabs

--- client/src/test/java/uk/co/blackpepper/bowman/LinkedResourceMethodHandlerTest.java
+++ client/src/test/java/uk/co/blackpepper/bowman/LinkedResourceMethodHandlerTest.java
@@ -16,133 +16,133 @@ import static org.springframework.util.ReflectionUtils.findMethod;
 
 public class LinkedResourceMethodHandlerTest {
 
-    private final ResourceContent resourceContent = new ResourceContent();
-    private final LinkedResourceMethodHandler handler = createHandler();
+	private final ResourceContent resourceContent = new ResourceContent();
+	private final LinkedResourceMethodHandler handler = createHandler();
 
-    @Test
-    public void supportsLinkedResourceSetterIsTrue() {
-        assertThat(handler.supports(findMethod(ResourceContent.class, "setSomething", String.class)), is(true));
-    }
+	@Test
+	public void supportsLinkedResourceSetterIsTrue() {
+		assertThat(handler.supports(findMethod(ResourceContent.class, "setSomething", String.class)), is(true));
+	}
 
-    @Test
-    public void supportsLinkedResourceGetterIsTrue() {
-        assertThat(handler.supports(findMethod(ResourceContent.class, "getSomething")), is(true));
-    }
+	@Test
+	public void supportsLinkedResourceGetterIsTrue() {
+		assertThat(handler.supports(findMethod(ResourceContent.class, "getSomething")), is(true));
+	}
 
-    @Test
-    public void supportsLinkedResourceSetterWithNoParamIsFalse() {
-        assertThat(handler.supports(findMethod(ResourceContent.class, "setNothing")), is(false));
-    }
+	@Test
+	public void supportsLinkedResourceSetterWithNoParamIsFalse() {
+		assertThat(handler.supports(findMethod(ResourceContent.class, "setNothing")), is(false));
+	}
 
-    @Test
-    public void supportsLinkedResourceVoidGetterIsFalse() {
-        assertThat(handler.supports(findMethod(ResourceContent.class, "getNothing")), is(false));
-    }
+	@Test
+	public void supportsLinkedResourceVoidGetterIsFalse() {
+		assertThat(handler.supports(findMethod(ResourceContent.class, "getNothing")), is(false));
+	}
 
-    @Test
-    public void supportsLinkedResourceSetterWithReturnValueIsFalse() {
-        assertThat(handler.supports(findMethod(
-                ResourceContent.class, "setSomethingWithReturn", String.class)), is(false));
-    }
+	@Test
+	public void supportsLinkedResourceSetterWithReturnValueIsFalse() {
+		assertThat(handler.supports(findMethod(
+				ResourceContent.class, "setSomethingWithReturn", String.class)), is(false));
+	}
 
-    @Test
-    public void supportsLinkedResourceSetterWithMultipleParamsIsFalse() {
-        assertThat(handler.supports(findMethod(
-                ResourceContent.class, "setSomethingWithExtraParam", String.class, String.class)), is(false));
-    }
+	@Test
+	public void supportsLinkedResourceSetterWithMultipleParamsIsFalse() {
+		assertThat(handler.supports(findMethod(
+				ResourceContent.class, "setSomethingWithExtraParam", String.class, String.class)), is(false));
+	}
 
-    @Test
-    public void supportsLinkedResourceGetterWithParamsIsFalse() {
-        assertThat(handler.supports(findMethod(
-                ResourceContent.class, "getSomethingFromParam", String.class)), is(false));
-    }
+	@Test
+	public void supportsLinkedResourceGetterWithParamsIsFalse() {
+		assertThat(handler.supports(findMethod(
+				ResourceContent.class, "getSomethingFromParam", String.class)), is(false));
+	}
 
-    @Test
-    public void supportsNonAnnotatedSetterIsFalse() {
-        assertThat(handler.supports(findMethod(ResourceContent.class, "setSomethingElse", String.class)), is(false));
-    }
+	@Test
+	public void supportsNonAnnotatedSetterIsFalse() {
+		assertThat(handler.supports(findMethod(ResourceContent.class, "setSomethingElse", String.class)), is(false));
+	}
 
-    @Test
-    public void supportsNonAnnotatedGetterIsFalse() {
-        assertThat(handler.supports(findMethod(ResourceContent.class, "getSomethingElse")), is(false));
-    }
+	@Test
+	public void supportsNonAnnotatedGetterIsFalse() {
+		assertThat(handler.supports(findMethod(ResourceContent.class, "getSomethingElse")), is(false));
+	}
 
-    @Test
-    public void invokeSetsAndReturnsSameLinkedResource() throws InvocationTargetException, IllegalAccessException {
-        final Method setterMethod = findMethod(ResourceContent.class, "setSomething", String.class);
-        assertThat(setterMethod, is(notNullValue()));
+	@Test
+	public void invokeSetsAndReturnsSameLinkedResource() throws InvocationTargetException, IllegalAccessException {
+		final Method setterMethod = findMethod(ResourceContent.class, "setSomething", String.class);
+		assertThat(setterMethod, is(notNullValue()));
 
-        final String setterValue = "STRING";
-        final Object setterResult = handler.invoke(resourceContent, setterMethod, null, new String[]{setterValue});
+		final String setterValue = "STRING";
+		final Object setterResult = handler.invoke(resourceContent, setterMethod, null, new String[]{setterValue});
 
-        assertThat(setterResult, is(notNullValue()));
-        assertThat(setterResult, instanceOf(String.class));
-        assertThat(setterValue, equalTo(setterResult));
+		assertThat(setterResult, is(notNullValue()));
+		assertThat(setterResult, instanceOf(String.class));
+		assertThat(setterValue, equalTo(setterResult));
 
-        final Method getterMethod = findMethod(ResourceContent.class, "getSomething");
-        assertThat(getterMethod, is(notNullValue()));
+		final Method getterMethod = findMethod(ResourceContent.class, "getSomething");
+		assertThat(getterMethod, is(notNullValue()));
 
-        final Object getterResult = handler.invoke(resourceContent, getterMethod, null, null);
+		final Object getterResult = handler.invoke(resourceContent, getterMethod, null, null);
 
-        assertThat(getterResult, is(notNullValue()));
-        assertThat(getterResult, instanceOf(String.class));
-        assertThat(getterResult, equalTo(setterResult));
+		assertThat(getterResult, is(notNullValue()));
+		assertThat(getterResult, instanceOf(String.class));
+		assertThat(getterResult, equalTo(setterResult));
 
-        assertThat(setterValue, equalTo(resourceContent.getSomething()));
-    }
+		assertThat(setterValue, equalTo(resourceContent.getSomething()));
+	}
 
-    private LinkedResourceMethodHandler createHandler() {
-        final JavassistClientProxyFactory proxyFactory = new JavassistClientProxyFactory();
-        return new LinkedResourceMethodHandler(new Resource<>(resourceContent),
-                new RestOperationsFactory(Configuration.builder().build(), proxyFactory).create(), proxyFactory);
-    }
+	private LinkedResourceMethodHandler createHandler() {
+		final JavassistClientProxyFactory proxyFactory = new JavassistClientProxyFactory();
+		return new LinkedResourceMethodHandler(new Resource<>(resourceContent),
+				new RestOperationsFactory(Configuration.builder().build(), proxyFactory).create(), proxyFactory);
+	}
 
-    @SuppressWarnings("ALL")
-    private static class ResourceContent {
-        String something;
+	@SuppressWarnings("ALL")
+	private static class ResourceContent {
+		String something;
 
-        @LinkedResource
-        public void setSomething(String value) {
-            something = value;
-        }
+		@LinkedResource
+		public void setSomething(String value) {
+			something = value;
+		}
 
-        @LinkedResource
-        public String getSomething() {
-            return something;
-        }
+		@LinkedResource
+		public String getSomething() {
+			return something;
+		}
 
-        @LinkedResource
-        public void setNothing() {
-            // no-op
-        }
+		@LinkedResource
+		public void setNothing() {
+			// no-op
+		}
 
-        @LinkedResource
-        public void getNothing() {
-            // no-op
-        }
+		@LinkedResource
+		public void getNothing() {
+			// no-op
+		}
 
-        @LinkedResource
-        public String setSomethingWithReturn(String value) {
-            return value;
-        }
+		@LinkedResource
+		public String setSomethingWithReturn(String value) {
+			return value;
+		}
 
-        @LinkedResource
-        public void setSomethingWithExtraParam(String value, String extraParam) {
-            // no-op
-        }
+		@LinkedResource
+		public void setSomethingWithExtraParam(String value, String extraParam) {
+			// no-op
+		}
 
-        @LinkedResource
-        public String getSomethingFromParam(String value) {
-            return value;
-        }
+		@LinkedResource
+		public String getSomethingFromParam(String value) {
+			return value;
+		}
 
-        public void setSomethingElse(String value) {
-            // no-op
-        }
+		public void setSomethingElse(String value) {
+			// no-op
+		}
 
-        public String getSomethingElse() {
-            return null;
-        }
-    }
+		public String getSomethingElse() {
+			return null;
+		}
+	}
 
 }
