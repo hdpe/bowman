770e96763ff95897a7c461bdec750d0a658e66d1
dana poklepovich <dana.poklepovich@gmail.com>
2018-07-10 11:12:12 +0800

    remove ignore annotation on ut

--- client/src/test/java/uk/co/blackpepper/bowman/JavassistClientProxyFactoryTest.java
+++ client/src/test/java/uk/co/blackpepper/bowman/JavassistClientProxyFactoryTest.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -269,7 +268,6 @@ public class JavassistClientProxyFactoryTest {
 	}
 	
 	@Test
-	@Ignore
 	public void createWithResourceWithProxiedInterfaceContentReturnsProxyWithLinkedResources() throws Exception {
 		InterfaceTypeResource content = instantiateProxyOfInterfaceType(InterfaceTypeResource.class);
 		
